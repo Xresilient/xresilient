7ece4b69019b645b2f19bdc7ea1c164e8215fb8f
Xresilient <124053427+Xresilient@users.noreply.github.com>
2023-06-02 14:08:05 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,6 +118,11 @@ const Index = () => {
             description="With our token development services, you can transform your business, unlock new revenue streams, and engage with a global community of digital enthusiasts. Discover the power of tokens with Xresilient, your trusted partner in blockchain innovation."
           />
           <SmallServiceCard />
+          <SmallServiceCard
+            image="https://source.unsplash.com/300x300/?web3"
+            title="NFT Designin, Development"
+            description="With our NFT design and development services, you can unleash your creativity, establish digital ownership, and engage with a global community of collectors and enthusiasts. Discover the power of NFTs with Xresilient, your trusted partner in unlocking the potential of the digital art and collectibles market."
+          />
           <SmallServiceCard />
         </div>
         <div className="grid rid-cols-1 md:grid-cols-2 gap-8">
