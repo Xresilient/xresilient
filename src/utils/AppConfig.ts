7cb8bbae833de1d0a600da20023653dce25abf8b
Sangam511 <patilsangam184@gmail.com>
2023-09-14 12:29:52 +0530

    added

--- src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-// FIXME: Update this configuration file based on your project information
-
-export const AppConfig = {
-  site_name: "Xresilient",
-  title: "Xresilient",
-  description: "Xresilient",
-  locale: "en",
-};
